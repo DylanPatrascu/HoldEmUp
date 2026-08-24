f9dccf8022b901d20eff9d21c7254f4863ad7a78
kaimaquivar <kmaquivar@outlook.com>
2026-08-23 21:08:20 -0400

    temp game over scene

---
+++ Assets/Scripts/UI/GameOverScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void _return_to_menu()
+    {
+
+    }
+}
