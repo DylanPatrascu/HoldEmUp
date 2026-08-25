227427c5a4d933c51d1dd26529358dae472a6727
Abdulrahman M. <53632818+Aoof@users.noreply.github.com>
2026-08-25 13:19:41 -0400

    patch: directory management

---
+++ Assets/Scripts/Managers/FPSCameraManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 100f;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xRotation = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (!playerBody) playerBody = transform.parent.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        xRotation -= mouseY;
+        
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
